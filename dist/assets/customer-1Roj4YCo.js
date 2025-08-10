e3229d187a7d3131cc06b80e2881edfe88f0f4f7
Doan Giang <dvg8.vcc@gmail.com>
2025-08-11 00:10:44 +0700

    update

---
+++ dist/assets/customer-1Roj4YCo.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-LmRE4l5i.js";const s=o.create({baseURL:"https://api.goldenwin.vn/api",withCredentials:!0}),r=(t={})=>s.get("/customers",{params:t}),c=t=>s.get(`/customers/${t}`),u=t=>s.post("/customers",t),m=(t,e)=>s.put(`/customers/${t}`,e),n=t=>s.delete(`/customers/${t}`);export{c as a,u as c,n as d,r as g,m as u};
--- dist/assets/purchaseHistory-DxsmAiGA.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./index-q_THj4Qt.js";const t=a.create({baseURL:"https://api.goldenwin.vn/api",withCredentials:!0}),o=(s={})=>t.get("/purchase-history",{params:s}),i=s=>t.post("/purchase-history",s),c=(s,e)=>t.put(`/purchase-history/${s}`,e);export{i as c,o as g,c as u};
